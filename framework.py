1271d5947ad92c77fe55f6f7d852a5def69af9a0
gokuldharan <gokul.dharan@mail.utoronto.ca>
2021-03-08 20:40:09 -0800

    Add distortion computation

--- framework.py
+++ framework.py
@@ -11,7 +11,6 @@ class DiscretizedDecisionSpace:
         return iter(self.dSpace)
 
 
-
 class Agent:
     def __init__(self, decisionSpace, utilityFn = random.random, disutilityScale = 1):
         self.decisionSpace = decisionSpace
@@ -21,11 +20,9 @@ class Agent:
         self.scale = disutilityScale
 
     def disutility(self, a):
-        return self.scale * (self.utility[self.blissPoint] - self.utility[a])
+        return self.scale * (self.utility[self.blissPoint] - self.utility[a]) # potentially needs to change for other decision spaces
 
     #Potentially implement utility shifting after bargaining?
-
-
 
 
 class SequentialDeliberation:
@@ -35,6 +32,18 @@ class SequentialDeliberation:
         self.agents = [Agent(decisionSpace, utilityFn = random.random) for i in range(numAgents)]
         self.bargainFn = bargainFn
         self.a = [self.agents[0].blissPoint] #keeping a list since it might be interesting to see how "a^t" progresses
+        self.altToSC = {}
+        self.optSC = np.inf
+        for alternative in self.decisionSpace:          #caching this assuming these don't get too large
+            sc = 0
+            for agent in self.agents:
+                sc += agent.disutility(alternative)
+            self.altToSC[alternative] = sc
+            self.optSC = min(self.optSC, sc)
+
+
+    def getDistortion(self, a):
+        return self.altToSC[a] / self.optSC
 
     def step(self):
         u, v = random.choices(self.agents, k=2) #sample w/ replacement
@@ -43,8 +52,11 @@ class SequentialDeliberation:
 
     def deliberate(self, horizon):
         for i in range(horizon):
+            print("Distortion at step " + str(i) + ": " + str(self.getDistortion(self.a[-1])))
             self.step()
+        print("Final Distortion :" + str(self.getDistortion(self.a[-1])))
         return self.a
+
 
 
 def nashBargain(u, v, a):
@@ -66,7 +78,6 @@ def nashBargain(u, v, a):
 
 
 
-
-sd = SequentialDeliberation(10, DiscretizedDecisionSpace(0.1), nashBargain)
+sd = SequentialDeliberation(1000, DiscretizedDecisionSpace(0.1), nashBargain)
 a = sd.deliberate(10)
 print(a)
